7fc3482679c3f0152cab2b189ccf954a48239a82
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-08-24 23:14:47 +0530

    Added getStaticProps and getStaticPaths for dynamic Pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,31 @@ function DetailsPage() {
   );
 }
 
+export async function getStaticPaths(){
+  return {
+    paths: [
+      {params: {meetupId: 'first'}},
+      {params: {meetupId: 'second'}},
+    ],
+    fallback: false
+  }
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
+        title: "First Meetup",
+        description: "This is the first Meetup",
+        address: "1234, City Street, California",
+      },
+    },
+  };
+}
+
 export default DetailsPage;
