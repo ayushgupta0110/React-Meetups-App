6005cee2b9e0f49e007fac9e4ff7fe3f4557a683
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-08-27 13:52:29 +0530

    Added mongoDB database connection

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,24 @@
 // /api/new-meetup
 
-function handler(req,res) {
+async function handler(req, res) {
+  if (req.method == "POST") {
+    const data = req.body;
 
-    if(req.method =='POST'){
-        const data = req.body;
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin-ayush:Ag747492@@@cluster0.2u0iu.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const collection = db.collection("meetups");
 
-        const {title,image,description,address} = data;
+    const result = await collection.insertOne(data);
+    console.log(result);
 
-    }
+    client.close();
+    
+    res.status(201).json({
+      message: "New meetup created successfully",
+    });
+  }
 }
 
 export default handler;
